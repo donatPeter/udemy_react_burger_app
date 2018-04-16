a49abe22f912eed7f051c724cf0daba251ce3f85
donatPeter <peterdonat.bme@gmail.com>
2018-04-16 17:56:37 +0200

    adding buildControl component to control adding ingredients

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './BuildControl.css'
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+
+  </div>
+);
+
+export default buildControl;
