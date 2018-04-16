0f3532a25167a7c4d85ef0c3e9a2ccc67a145886
donatPeter <peterdonat.bme@gmail.com>
2018-04-16 17:02:10 +0200

    now burger ingredients come from the builder state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+  const ingredientsArray = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])].map((_, i) => {
+        return <BurgerIngredient key={key + i} type={key} />
+      })
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {ingredientsArray}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
