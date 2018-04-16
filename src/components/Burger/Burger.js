08da16276910ef42dc22aff8f3885169876d190f
donatPeter <peterdonat.bme@gmail.com>
2018-04-16 17:09:50 +0200

    adding notification in case of zero ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-  const ingredientsArray = Object.keys(props.ingredients)
+  let ingredientsArray = Object.keys(props.ingredients)
     .map(key => {
       return [...Array(props.ingredients[key])].map((_, i) => {
         return <BurgerIngredient key={key + i} type={key} />
       })
-    });
+    })
+    .reduce((prev, curr) => { // reduce the two dimensional array to get an array of ingredients
+      return prev.concat(curr);
+    }, [])
+
+  if (ingredientsArray.length === 0) {
+    ingredientsArray = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={classes.Burger}>
