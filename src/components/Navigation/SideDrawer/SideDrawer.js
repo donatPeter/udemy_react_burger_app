b48b1749087fc4b7e84d1a8191e31a46004d1cbf
donatPeter <peterdonat.bme@gmail.com>
2018-04-17 18:40:23 +0200

    sideDrawer on mobil

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import BackDrop from '../../UI/BackDrop/BackDrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
 
+  let attachedClasses = [classes.SideDrawer, classes.Close].join(' ');
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open].join(' ');;
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
