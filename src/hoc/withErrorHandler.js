cacfe856d55357873b34fe812cd37d30c3568a77
donatPeter <peterdonat.bme@gmail.com>
2018-04-18 13:25:25 +0200

    removing axios interceptops on component unmounting

--- src/hoc/withErrorHandler.js
+++ src/hoc/withErrorHandler.js
@@ -11,13 +11,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
     }
 
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInt = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInt = axios.interceptors.response.use(res => res, error => {
         this.setState({ error: error });
       });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.response.eject(this.resInt);
+      axios.interceptors.request.eject(this.reqInt);
     }
 
     errorConfirmedHandler = () => {
