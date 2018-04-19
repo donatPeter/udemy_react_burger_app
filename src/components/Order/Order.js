185f0c5ccebe27412480059a0e78741c4fe4a001
donatPeter <peterdonat.bme@gmail.com>
2018-04-19 16:05:10 +0200

    adding previous orders page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = () => (
+  <div className={classes.Order}>
+    <p>Ingredients:</p>
+    <p>Price:</p>
+  </div>
+);
+
+export default order;
