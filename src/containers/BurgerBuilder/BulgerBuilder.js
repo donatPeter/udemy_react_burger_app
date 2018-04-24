75f0fd6b5f929e1c2459d3e42862c061a1aeb9e8
donatPeter <peterdonat.bme@gmail.com>
2018-04-24 09:58:11 +0200

    use redux in checkout and contact data components

--- src/containers/BurgerBuilder/BulgerBuilder.js
+++ src/containers/BurgerBuilder/BulgerBuilder.js
@@ -48,16 +48,7 @@ class BurgerBuilder extends Component {
   }
 
   orderContinueHandler = () => {
-    const queryParams = [];
-    for (let ingredient in this.props.ings) {
-      queryParams.push(encodeURIComponent(ingredient) + '=' + encodeURIComponent(this.props.ings[ingredient]));
-    }
-    queryParams.push('price=' + this.props.price);
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString
-    });
+    this.props.history.push('/checkout');
   }
 
   render() {
